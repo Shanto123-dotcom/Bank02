4d0e8882fb63cc4efd140c4c47ac03836daa14b0
Shanto123-dotcom <79821487+Shanto123-dotcom@users.noreply.github.com>
2021-03-07 17:21:23 +0600

    Account Class Work Half Done

--- Account.cs
+++ Account.cs
@@ -8,6 +8,82 @@ namespace B02
 {
     class Account
     {
+        private string dateOfBirth;
+        private int accountNumber;
+        private string accountName;
+        private double balance;
+        private string address;
+        private string accountType;
+        static int next = 3300;
+        private int transactions = 0;
+
+        public string Address
+        {
+            get { return this.address; }
+            set { this.address = value; }
+        }
+        public string AccountName
+        {
+            get { return this.accountName; }
+            set { this.accountName = value; }
+        }
+        public int Transactions
+        {
+            get { return this.transactions; }
+        }
+
+        public double Balance
+        {
+            get { return this.balance; }
+            set { this.balance = value; }
+        }
+
+        public string DateOfBirth
+        {
+            get { return this.dateOfBirth; }
+            set { this.dateOfBirth = value; }
+        }
+
+        public string AccountType
+        {
+            get { return this.accountType; }
+            set { this.accountType = value; }
+        }
+
+
+        public void AutoAcoountNumberGenarator()
+        {
+            this.accountNumber = next;
+            next++;
+        }
+
+        public int AccountNumber
+        {
+            get { return this.accountNumber; }
+        }
+
+        virtual public void Withdraw(double amount)
+        {
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+        }
+
+        public void Transfer(double amount, Account receiver)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                receiver.Deposit(amount);
+            }
+        }
+
+       
 
     }
 }
